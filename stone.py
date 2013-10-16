37f4aad53639d332bc2c8adb461b59ea650d96e2
wangjohn <wangjohn@mit.edu>
2013-10-16 04:45:57 -0400

    Make stone work correctly. Using shapely

--- stone.py
+++ stone.py
@@ -1,33 +1,68 @@
-import polygon
 import random
+from shapely import geometry
 
 class Stone:
-    def __init__(self, polygon, angle_deviation = 15.0):
+    def __init__(self, polygon, fraction_mean = 0.5, fraction_deviation = 0.15):
         self.polygon = polygon
-        self.angle_deviation = angle_deviation
+        self.fraction_mean = fraction_mean
+        self.fraction_deviation = fraction_deviation
 
     def chip_vertex(self, index = None):
         if not index:
-            index = random.randrange(self.polygon.n)
-        vertex = self.polygon.get_vertex(index)
-        left_neighbor = self.polygon.get_vertex(index-1)
-        right_neighbor = self.polygon.get_vertex(index+1)
+            index = random.randrange(len(self.polygon.exterior.coords)-1)
 
-        max_length = self.max_chip_length(vertex, left_neighbor, right_neighbor)
-        angle = self.get_random_angle()
+        vertex = self.get_vertex(index)
+        left_neighbor = self.get_vertex(index-1)
+        right_neighbor = self.get_vertex(index+1)
+        left_line = geometry.LineString([left_neighbor, vertex])
+        right_line = geometry.LineString([right_neighbor, vertex])
 
-    def max_chip_length(self, vertex, left_neighbor, right_neighbor):
-        return float(min(vertex.distance(left_neighbor), vertex.distance(right_neighbor))) / 2.0
+        new_left_point = self.get_random_point_on_line(left_line)
+        new_right_point = self.get_random_point_on_line(right_line)
 
-    # Returns an angle between -90.0 and +90.0 based on a normal distribution
-    # centered with mean of 0.0 and standard deviation which is set by an input
-    # to the stone.
-    def get_random_angle(self):
-        angle = self._generate_potential_angle()):
-        while angle <= -90.0 or angle >= 90.0:
-            angle = self._generate_potential_angle()
+        new_exterior = list(self.polygon.exterior.coords)
+        new_exterior[index] = new_right_point
+        new_exterior.insert(index, new_left_point)
 
-        return angle
+        self.polygon = geometry.Polygon(new_exterior)
 
-    def _generate_potential_angle(self):
-        return random.normalvariate(0.0, self.angle_deviation)
+    def get_vertex(self, index):
+        return self.polygon.exterior.coords[index]
+
+    def get_random_point_on_line(self, line):
+        return line.interpolate(self.get_random_fraction(), True).coords[0]
+
+    def get_random_fraction(self):
+        fraction = self._generate_potential_fraction()
+        while fraction < 0 or fraction > 1:
+            fraction = self._generate_potential_fraction()
+
+        return fraction
+
+    def _generate_potential_fraction(self):
+        return random.normalvariate(self.fraction_mean, self.fraction_deviation)
+
+if __name__ == '__main__':
+    from matplotlib import pyplot
+    from descartes.patch import PolygonPatch
+
+    def plot_coords(ax, ob):
+        x, y = ob.xy
+        ax.plot(x, y, 'o', color='#999999', zorder=1)
+
+    fig = pyplot.figure(1, dpi=90)
+    for i in xrange(9):
+        square = geometry.box(0.0, 0.0, 1.0, 1.0)
+        stone = Stone(square, 0.1, 0.15)
+
+        for j in xrange(50):
+            stone.chip_vertex()
+
+        ax = fig.add_subplot(int('33' + str(i)))
+        plot_coords(ax, stone.polygon.exterior)
+        polygon = stone.polygon
+        patch = PolygonPatch(polygon, facecolor='#6699cc', edgecolor='#6699cc', alpha=0.5, zorder=2)
+        ax.add_patch(patch)
+        ax.set_xlim(*[-0.5,1.5])
+        ax.set_ylim(*[-0.5,1.5])
+    pyplot.show()
