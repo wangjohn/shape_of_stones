fe535b96d83df6aa8c1e9c7493e017bc8dfdbb20
Jonathan Allen <jallen01@mit.edu>
2013-10-27 00:09:52 -0400

    Using DFT not FFT.

--- continuous_surface/continuous_surface.py
+++ continuous_surface/continuous_surface.py
@@ -15,11 +15,12 @@ from numpy.polynomial import chebyshev
 
 
 PLOT_RESOLUTION = 1E3
-T = 0.1
+T = 0.5
 N_STEPS = T/0.01
 N_PTS = 20
+H_MIN = 0.2
 METHOD = ["remove_points", "remove_lowest"][0]
-SHAPE = ["ellipse", "blob"][1]
+SHAPE = ["circle", "ellipse", "blob"][2]
 
 def remove_points(shape):
     return arctan((shape.curvature()-1)*5) + pi/2
@@ -27,114 +28,208 @@ def remove_points(shape):
 def remove_lowest(shape):
     shape.extrema()
 
-def ellipse(theta):
-    x = 2*cos(theta)
-    y = sin(theta)
-    return SpectralShape(vstack([x, y]).T)
+def circle(s):
+    x = cos(s)
+    y = sin(s)
+    return SpectralShape(vstack([x, y]).T, s)
 
-def blob(theta):
-    x = cos(theta)*chebyshev.chebval((theta-pi)/pi, [2,0,1])
-    y = sin(theta)*chebyshev.chebval((theta-pi)/pi, [2,0,0,0,1])
-    return SpectralShape(vstack([x, y]).T)
+def ellipse(s):
+    x = 2*cos(s)
+    y = sin(s)
+    return SpectralShape(vstack([x, y]).T, s)
+
+def blob(s):
+    x = cos(s)*chebyshev.chebval((s-pi)/pi, [2,0,1])
+    y = sin(s)*chebyshev.chebval((s-pi)/pi, [2,0,0,0,1])
+    return SpectralShape(vstack([x, y]).T, s)
 
 
 # -----------------------------------------------------------------------------
+
+DIGITS = 53;
+TOL = sqrt(0.5**DIGITS)
+def ellipse_circumference(a, b):
+    """
+    Compute the circumference of an ellipse with semi-axes a and b.
+    Require a >= 0 and b >= 0.  Relative accuracy is about 0.5^53.
+    """
+    x = max(a, b)
+    y = min(a, b)
+    
+    if DIGITS*y < TOL*x: 
+        return 4 * x
+
+    s = 0
+    m = 1
+    while x-y > tol*y:
+        x = 0.5 * (x + y) 
+        y = sqrt(x * y)
+        m *= 2 
+        s += m * (x - y)**2
+    return pi * ((a + b)**2 - s) / (x + y)
 
 
 def frobenius_norm(x):
     return sqrt(sum(x**2, axis=-1))
 
-def get_spectral(x):
-    return fft.rfft(x)
+def apply_to_cols(f):
+    def func(mat, *args, **kwargs):
+        mat = mat.copy().reshape(len(mat), -1)
 
-def get_values(x_hat, theta):
-    k = arange(len(x_hat))
-    mat = (1/len(x_hat))*exp(complex(0,1)*theta[:,newaxis]*k)
-    return mat.dot(x_hat)
+        for i in arange(mat.shape[1]):
+            try:
+                out = vstack([out, f(mat[:,i], *args, **kwargs)])
+            except NameError:
+                out = f(mat[:,i], *args, **kwargs)
 
-def spectral_derivative(x, n=1):
-    n_pts = x.shape[0]
-    x_col = x.reshape(n_pts, -1)
-    w = zeros(x_col.shape)
+        return out.T
 
-    for i in arange(x.shape[-1]):
-        w_hat = fft.rfft(x_col[:,i]) * (complex(0,1)*arange(int(n_pts/2+1)))**n
-        if n % 2 == 1:
-            w_hat[-1] = 0
-        w[:,i] = fft.irfft(w_hat)
+    return func
 
-    return w.reshape(x.shape)
+@apply_to_cols
+def get_spectral(x, s):
+    n_pts = len(s)
+    k = arange(n_pts/2+1)
 
-def spectral_integral(x, n=1):
-    n_pts = x.shape[0]
-    x_col = x.reshape(n_pts, -1)
-    w = zeros(x_col.shape)
+    mat = exp(1j*s*k[:,newaxis])
 
-    for i in arange(x_col.shape[1]):
-        w_hat = fft.rfft(x_col[:,i]) * (1/(complex(0,1)*arange(int(n_pts/2+1)))**n)
-        if n % 2 == 1:
-            w_hat[-1] = 0
-        w[:,i] = fft.irfft(w_hat)
-    return w.reshape(x.shape)
+    return mat.dot(x)
 
-def spectral_interpolation(x, min_n_pts):
-    x_hat = fft.rfft(x)
-    if len(x) < min_n_pts:
-        x_hat = append(x_hat, zeros((min_n_pts - len(x))/2))
+@apply_to_cols
+def get_values(x_hat, s):
+    x_hat_full = hstack([x_hat, conjugate(x_hat[-2:0:-1])])
+    n_pts = int((len(x_hat)-1)*2)
+
+    k = hstack([arange(n_pts/2+1), arange(-n_pts/2+1, 0)])
+    mat = (1/n_pts)*exp(1j*s[:,newaxis]*k)
+
+    return real(mat.dot(x_hat_full))
+
+@apply_to_cols
+def spectral_interpolate(x, s, s_new):
+    return get_values(get_spectral(x, s), s_new)
+
+@apply_to_cols
+def spectral_derivative(x, s, n=1):
+    n_pts = len(s)
+
+    k = arange(int(n_pts/2+1))
+    x_hat = get_spectral(x, s) 
+    w_hat = x_hat * (1j*k)**n
+    if n % 2 == 1:
+        w_hat[-1] = 0
+
+    return get_values(w_hat, s)
+
+@apply_to_cols
+def spectral_integral(x, s, n=1, endpoint=False):
+    n_pts = len(s)
+
+    k = arange(int(n_pts/2+1))
+    x_hat = get_spectral(x, s)
+    w_hat = x_hat * hstack([0, (1/(1j*k[1:])**n)])
+    if n % 2 == 1:
+        w_hat[-1] = 0
     
-    x_fine = fft.irfft(x_hat)
-    x_fine *= len(x_fine)/len(x)
-    return x_fine
+    if endpoint:
+        s = linspace(0, 2*pi, n_pts+1)
+    else:
+        s = linspace(0, 2*pi, n_pts, endpoint=False)
 
-def plot_spectral(x):
-        resolution = maximum(len(self.x), self.PLOT_RESOLUTION)
-        s_fine = linspace(0, 2*pi, resolution, endpoint=False)
-        x_fine = spectral_interpolation(self.x, resolution)
-        plot(s_fine, x_fine)
+    const = (1/n_pts)*s*real(x_hat[0])
+    
+    return get_values(w_hat, s) + const
+
+@apply_to_cols
+def improve_resolution(x, s, min_pts):
+    s_new = linspace(0, 2*pi, max([min_pts, len(x)]), endpoint=False)
+    return spectral_interpolate(x, s, s_new)
+
+def plot_spectral(s, x):
+    s_fine = linspace(0, 2*pi, PLOT_RESOLUTION)
+    x_fine = spectral_interpolate(x, s, s_fine)
+    plot(s_fine, x_fine)
 
 
-class SpectralShape:
-    def __init__(self, x):
-        self.set_x(x)
+class SpectralShape(object):
+    def __init__(self, x, s, h_min=H_MIN):
+        self.h_min = h_min
+        self.s = s
+        self.x = x
 
-    def set_x(self, x):
-        self.x = x.reshape(-1, 2)
+    def __len__(self):
+        return len(self.s)
+
+    def index_to_full(self, index):
+        return nonzero(self._valid_points)[0][index]
+
+    def ignore_point(self, index):
+        self._valid_points[self.index_to_full(index)] = False
+
+    def convert_to_partial(self, mat):
+        return mat[self._valid_points]
+
+    def convert_to_full(self, mat):
+        out = self._x.copy()
+        out[self._valid_points] = mat
+        return out
+
+    @property
+    def s(self):
+        return self.convert_to_partial(self._s)
+
+    @s.setter
+    def s(self, value):
+        self._s = value.copy()
+        if not hasattr(self, '_valid_points'):
+            self._valid_points = ones(len(self._s), dtype=bool)
+
+    @property
+    def x(self):
+        return self.convert_to_partial(self._x)
+
+    @x.setter
+    def x(self, value):
+        self._x = value.copy().reshape(-1, 2)
+        self.redistribute()
 
     def surface_normal(self):
-        x_dot = spectral_derivative(self.x, 1)
+        x_dot = spectral_derivative(self.x, self.s, n=1)
         x_dot_n = x_dot[:,(1,0)]
         x_dot_n *= [-1,1]
         x_dot_n /= frobenius_norm(x_dot_n)[:,newaxis]
         return x_dot_n
 
     def curvature(self):
-        x_dot = spectral_derivative(self.x, 1)
-        x_ddot = spectral_derivative(self.x, 2)
+        x_dot = spectral_derivative(self.x, self.s, n=1)
+        x_ddot = spectral_derivative(self.x, self.s, n=2)
             
         k = cross(x_dot, x_ddot) / frobenius_norm(x_dot)**3
         return k
 
-    def redistribute(self, n_pts=N_PTS):
-        x_dot = spectral_derivative(self.x, 1)
-        v = frobenius_norm(x_dot)
-        L = spectral_integral(v)
+    def redistribute(self):
+        v = frobenius_norm(spectral_derivative(self.x, self.s, n=1))
+        l = spectral_integral(v, self.s)
+        dl = diff(l)
 
-        v *= (2*pi)/L # Rescale path length to 2*pi
-        theta_hat = get_spectral(spectral_integral(1/v))
-        theta = get_values(theta_hat, linspace(0, 2*pi, n_pts, endpoint=False))
+        while True:
+            small = nonzero(dl < self.h_min)[0]
+            if len(small) == 0:
+                break
 
-        self.set_x(get_values(get_spectral(self.x), theta))
+            index = small[0]
+            dl[index+1] += dl[index]
+
+            dl = delete(dl, index)
+            self.ignore_point(index+1)
 
     def dxdt(self, method):
         return method(self)[:,newaxis] * self.surface_normal()
 
     def plot(self):
-        resolution = maximum(len(self.x), PLOT_RESOLUTION)
-        x_fine = zeros([resolution, 2])
-        for i in arange(2):
-            x_fine[:,i] = spectral_interpolation(self.x[:,i], resolution)
-        
-        x_fine = vstack([x_fine, x_fine[0]])
+        s_fine = linspace(0, 2*pi, PLOT_RESOLUTION, endpoint=False)
+        x_fine = spectral_interpolate(self.x, self.s, s_fine)
+
         plot(x_fine[:,0], x_fine[:,1])
         plot(self.x[:,0], self.x[:,1], 'x')
         axis('equal')
@@ -143,27 +238,28 @@ class SpectralShape:
 # -----------------------------------------------------------------------------
 
 
-def run_simulation(shape, t, method):
-    def func(x, t):
-        shape.set_x(x)
-        return shape.dxdt(method).flatten()
-
-    x_simulation = integrate.odeint(func, shape.x.flatten(), t).reshape(len(t), -1, 2)
+def run_simulation(shape, t_steps, method):
     
+    
+    def func(x, t):
+        shape.x = x
+        return shape.convert_to_full(shape.dxdt(method)).flatten()
+
+    shape.plot()
+    figure()
+
+    x_simulation = integrate.odeint(func, shape.x.flatten(), t_steps).reshape(len(t_steps), -1, 2)
 
     for i in arange(N_STEPS, step=int(N_STEPS/4)):
-        shape.set_x(x_simulation[i])
+        shape.x = x_simulation[i]
         shape.plot()
 
     show()
 
 
 method = {"remove_points": remove_points, "remove_lowest": remove_lowest}[METHOD]
-theta = linspace(0, 2*pi, N_PTS, endpoint=False)
-shape = {"ellipse": ellipse, "blob": blob}[SHAPE](theta)
+s = linspace(0, 2*pi, N_PTS, endpoint=False)
+shape = {"circle":circle, "ellipse": ellipse, "blob": blob}[SHAPE](s)
 t = linspace(0, T, N_STEPS)
 
-#run_simulation(shape, t, method)
-
-shape.redistribute()
-shape.plot()
+run_simulation(shape, t, method)
